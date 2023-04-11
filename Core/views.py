c9eead036f30be9959bfb510520f9d59e4b007a2
Vitor dos Santos Gomes <54445515+santosgv@users.noreply.github.com>
2023-04-11 19:17:36 -0300

    novo template

--- Core/views.py
+++ Core/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-from .models import Produto
+from .models import Produto,Categoria
 
 def index(response):
+    Categorias = Categoria.objects.all()
     Produtos = Produto.objects.all()
     print(Produtos)
-    return render(response,'index.html',{'Produtos':Produtos})
+    return render(response,'index.html',{'Produtos':Produtos,
+                                         'Categorias':Categorias})
 
 def produto(response):
     return render(response,'templates/home')
