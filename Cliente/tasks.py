f9d71be115fbd5b86d22a48614888f0c583eb2d8
Vitor <santosgomesv@gmail.com>
2023-04-27 11:58:41 -0300

    task

--- Cliente/tasks.py
+++ Cliente/tasks.py
@@ -1,10 +1,25 @@
 from celery import shared_task
-from django_tenants.utils import tenant_context
+from django_tenants.utils import tenant_context,get_tenant_model
 from django.utils import timezone
 from .models import Cliente
 from django.contrib.auth.models import User
 from django.core.mail import send_mail
 from decouple import config
+from celery import Celery
+
+class TenantAwareCelery(Celery):
+    def execute(self, *args, **kwargs):
+        tenant_id = kwargs.pop('tenant_id', None)
+        if tenant_id is not None:
+            tenant_model = get_tenant_model()
+            try:
+                tenant = tenant_model.objects.get(id=tenant_id)
+            except tenant_model.DoesNotExist:
+                raise ValueError(f"Tenant {tenant_id} does not exist")
+            set_current_tenant(tenant)
+            set_tenant_schema_name(tenant.schema_name)
+        return super().execute(*args, **kwargs)
+
 
 @shared_task
 def Envia_email_com_super_usuario(client):
