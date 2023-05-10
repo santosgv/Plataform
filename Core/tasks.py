ffaeff979a82eb3c419619700063997879afa4be
Vitor <santosgomesv@gmail.com>
2023-05-10 20:01:24 -0300

    removendo task de envio de email

--- Core/tasks.py
+++ Core/tasks.py
@@ -6,7 +6,7 @@ from decouple import config
 def enviar_email_novo_pedido(pedido_id):
     subject = 'Novo pedido criado'
     message = f'O pedido {pedido_id} foi criado com sucesso!'
-    from_email = config('EMAIL_HOST_USER')
-    recipient_list = [config('recipient_list')]
-    send_mail(subject, message, from_email, recipient_list, fail_silently=False)
-    return message
+    #from_email = config('EMAIL_HOST_USER')
+    #recipient_list = [config('recipient_list')]
+    #send_mail(subject, message, from_email, recipient_list, fail_silently=False)
+    #return message
