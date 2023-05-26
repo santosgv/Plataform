b22ab48413615fc4215b81c8b467d3296b025221
Vitor <santosgomesv@gmail.com>
2023-05-26 00:22:13 -0300

    volta

--- Plataform/settings.py
+++ Plataform/settings.py
@@ -13,19 +13,8 @@ SECRET_KEY =config('SECRET_KEY')
 
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-if not DEBUG:
-	SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-	SECURE_SSL_REDIRECT = True
-	SESSION_COOKIE_SECURE = True
-	CSRF_COOKIE_SECURE = True
 
-APSCHEDULER_DATETIME_FORMAT = "N j, Y, f:s a"
-APSCHEDULER_RUN_NOW_TIMEOUT = 25
-
-
-ALLOWED_HOSTS = ['*']
-
-SCHEDULER_AUTOSTART=True
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -39,45 +28,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'debug_toolbar',
     'corsheaders',
-    'django_celery_beat',
     'Core',
 ]
 
-SHARED_APPS = [
-    'django_tenants',  
-    'Cliente',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'debug_toolbar',
-    'corsheaders',
-    'django_celery_beat',
-    'Core', 
-]
-
-TENANT_APPS = [
-    # The following Django contrib apps must be in TENANT_APPS
-    'django.contrib.contenttypes',
-    'django.contrib.auth',
-    'django.contrib.admin',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'corsheaders',
-    # tenant-specific apps
-     'Core',
-]
-INSTALLED_APPS = list(SHARED_APPS) + [
-    app for app in TENANT_APPS if app not in SHARED_APPS
-]
-
 MIDDLEWARE = [
-    'django_tenants.middleware.main.TenantMainMiddleware',
-    #'Plataform.middleware.TenantActiveMiddleware',
-    #'Plataform.middleware.RemoveWWWMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -90,8 +45,6 @@ MIDDLEWARE = [
 
 INTERNAL_IPS = [
     "127.0.0.1",
-    'localhost',
-    ''
 ]
 
 ROOT_URLCONF = 'Plataform.urls'
@@ -124,29 +77,11 @@ WSGI_APPLICATION = 'Plataform.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django_tenants.postgresql_backend',
-        'NAME': config('BANCO'),
-        'USER': config('BANCO_USER'),
-        'PASSWORD': config('BANCO_PASSWORD'),
-        'HOST': config('BANCO_HOST'),
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
-DATABASE_ROUTERS = (
-
-    'django_tenants.routers.TenantSyncRouter',
- )
-
-TENANT_MODEL = "Cliente.Cliente"
-
-TENANT_DOMAIN_MODEL = "Cliente.Domain"
-
-TENANT_COLOR_ADMIN_APPS = False
-
-
-#limita as chamadas no  search_path
-TENANT_LIMIT_SET_CALLS = True
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -203,10 +138,6 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'),)
 STATIC_ROOT = os.path.join('static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-
-# unifica os arquvios storege por inquelino
-#DEFAULT_FILE_STORAGE = 'tenant_schemas.storage.TenantFileSystemStorage'
-
 MEDIA_URL = '/media/'
 
 MESSAGE_TAGS = {
@@ -227,28 +158,12 @@ EMAIL_HOST='smtp.gmail.com'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
-# celery
-
-
-CELERY_BROKER_URL=config('CELERY_BROKER_URL')
-#CELERY_RESULT_BACKEND = 'redis://localhost:6379'
-CELERY_ACCEPT_CONTENT = ['json']
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_SERIALIZER = 'json'
-CELERY_TIMEZONE = 'America/Sao_Paulo'
-
-# celery Beat
-
-CELERY_BEAT_SCHEDULER ="django_celery_beat.schedulers:DatabaseScheduler"
-
-# AWS amazon
-AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
-AWS_S3_SIGNATURE_NAME = 's3v4',
-AWS_S3_REGION_NAME = 'sa-east-1'
-AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL =  None
-AWS_S3_VERITY = True
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+# AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+# AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+# AWS_S3_SIGNATURE_NAME = 's3v4',
+# AWS_S3_REGION_NAME = 'sa-east-1'
+# AWS_S3_FILE_OVERWRITE = False
+# AWS_DEFAULT_ACL =  None
+# AWS_S3_VERITY = True
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
