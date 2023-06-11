5d260f9170418aa4342dd536842e6edffa0253c8
Vitor <santosgomesv@gmail.com>
2023-06-11 15:49:38 -0300

    removendo o envio de email por cliente

--- Cliente/signals.py
+++ Cliente/signals.py
@@ -30,7 +30,7 @@ def created_user_client(sender, **kwargs):
         '''
         from_email = config('EMAIL_HOST_USER')
         recipient_list = [config('recipient_list')]
-        send_mail(subject, message, from_email, recipient_list, fail_silently=False)
+        #send_mail(subject, message, from_email, recipient_list, fail_silently=False)
         return 'Super Usuario Criado'
     
 
