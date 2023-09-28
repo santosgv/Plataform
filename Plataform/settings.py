4def776d35e03e393e583a567c23645f2a2c1b11
Vitor dos Santos Gomes <54445515+santosgv@users.noreply.github.com>
2023-09-28 16:00:46 -0300

    new variaveis

--- Plataform/settings.py
+++ Plataform/settings.py
@@ -225,7 +225,7 @@ EMAIL_HOST_USER= config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD= config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS=True
 EMAIL_PORT =587
-EMAIL_HOST='smtp.gmail.com'
+EMAIL_HOST='smtp.office365.com'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
